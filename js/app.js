78b271ab19b1c08cd8debf3bc4b93d2040323eaf
James Walsh <jameswalshdeveloper@protonmail.com>
2021-03-20 15:51:29 +0000

    Added modal functionality

---
+++ js/app.js
@@ -0,0 +1,20 @@
+//Selecting buttons and modal
+const modal = document.querySelector('.modal');
+const openModalBtn = document.getElementById('open-modal-btn');
+const closeModalBtn = document.querySelector('.close-modal-btn');
+
+//Listen for click on openModalBtn to open modal
+openModalBtn.addEventListener('click', openModal);
+
+//Function to open modal
+function openModal() {
+  modal.style.display = 'block';
+}
+
+//Listen for click on closeModalBtn to close modal
+closeModalBtn.addEventListener('click', closeModal);
+
+//Function to close modal
+function closeModal() {
+  modal.style.display = 'none';
+}
