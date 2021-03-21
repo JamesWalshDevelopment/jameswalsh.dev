82f021237599f4efac53b435743823f3f2a3cb2f
James Walsh <jameswalshdeveloper@protonmail.com>
2021-03-21 21:15:45 +0000

    Added navbar functionality

--- js/app.js
+++ js/app.js
@@ -18,3 +18,12 @@ closeModalBtn.addEventListener('click', closeModal);
 function closeModal() {
   modal.style.display = 'none';
 }
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
